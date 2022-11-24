2bfa43c104a51c4fb759d661352225d16c8bc0b8
Cristian Coronel Job <cristiancamilocoronelc800@gmail.com>
2022-11-24 09:02:59 -0500

    Changes in history

--- src/Interfaces/history.java
+++ src/Interfaces/history.java
@@ -1,21 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package Interfaces;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javaproject.parking.connectionDB;
+import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Usuario
- */
+
 public class history extends javax.swing.JFrame {
 
     int xMouse, yMouse;
     
+    connectionDB connection = new connectionDB("parking_son");
+    
+    DefaultTableModel model;
+    
     public history() {
         initComponents();
+        
+        jTable1.getTableHeader().setFont(new Font("Segoe UI", Font.BOLD, 14));
+        jTable1.getTableHeader().setOpaque(false);
+        jTable1.getTableHeader().setBackground(new Color(46, 125, 50));
+        jTable1.getTableHeader().setForeground(Color.white);
+        jTable1.setRowHeight(25);
+        
+        connection.connect();
+        
+        consult();
     }
 
     /**
@@ -42,7 +55,9 @@ public class history extends javax.swing.JFrame {
         headerPanel = new javax.swing.JPanel();
         exitButton = new javax.swing.JPanel();
         exitTextX = new javax.swing.JLabel();
-        background2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -320,20 +335,35 @@ public class history extends javax.swing.JFrame {
 
         backgroundPanel.add(headerPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
-        background2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setFont(new java.awt.Font("agave Nerd Font", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel1.setText("Vehículos en estacionamiento actualmente");
+        jLabel1.setToolTipText("");
+        backgroundPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 30, 550, 40));
 
-        javax.swing.GroupLayout background2Layout = new javax.swing.GroupLayout(background2);
-        background2.setLayout(background2Layout);
-        background2Layout.setHorizontalGroup(
-            background2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 880, Short.MAX_VALUE)
-        );
-        background2Layout.setVerticalGroup(
-            background2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 580, Short.MAX_VALUE)
-        );
+        jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
+        jScrollPane1.setForeground(new java.awt.Color(51, 51, 51));
 
-        backgroundPanel.add(background2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 0, 880, 580));
+        jTable1.setBackground(new java.awt.Color(255, 255, 255));
+        jTable1.setFont(new java.awt.Font("agave Nerd Font", 0, 12)); // NOI18N
+        jTable1.setForeground(new java.awt.Color(51, 51, 51));
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Placa", "Tipo de Vehículo", "Hora de Entrada", "Hora de Salida", "Costo del Estacionamiento"
+            }
+        ));
+        jTable1.setFocusable(false);
+        jTable1.setRowHeight(25);
+        jTable1.setSelectionBackground(new java.awt.Color(46, 125, 50));
+        jTable1.setSelectionForeground(new java.awt.Color(255, 255, 255));
+        jTable1.setShowGrid(false);
+        jTable1.setShowHorizontalLines(true);
+        jScrollPane1.setViewportView(jTable1);
+
+        backgroundPanel.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 90, 820, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -453,6 +483,36 @@ public class history extends javax.swing.JFrame {
     }//GEN-LAST:event_exitButtonTextPanelBarMousePressed
 
     
+    public void consult() {
+        String query = "select * from vehiculos";
+        
+        try {
+            
+            Statement st = connection.connect().createStatement();
+            ResultSet rs = st.executeQuery(query);
+            
+            Object[] vehInParking= new Object[5];
+            
+            model = (DefaultTableModel) jTable1.getModel();
+            
+            while(rs.next()) {
+                vehInParking [0] = rs.getString(1);
+                vehInParking [1] = rs.getString(2);
+                vehInParking [2] = rs.getString(3);
+                vehInParking [3] = rs.getString(4);
+                vehInParking [4] = rs.getString(5);
+                
+                model.addRow(vehInParking);
+            }
+            
+            jTable1.setModel(model);
+
+        } catch(SQLException e) {
+            System.err.println("No se ha podido realizar la consulta a la base de datos.");
+        }
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -464,7 +524,7 @@ public class history extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -489,7 +549,6 @@ public class history extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel background2;
     private javax.swing.JPanel backgroundPanel;
     private javax.swing.JPanel exitButton;
     private javax.swing.JPanel exitButtonPanelBar;
@@ -500,6 +559,9 @@ public class history extends javax.swing.JFrame {
     private javax.swing.JLabel historyButtonImg;
     private javax.swing.JPanel homeButton;
     private javax.swing.JLabel homeButtonImg;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JPanel panelBar;
     private javax.swing.JPanel vehiclesButton;
     private javax.swing.JLabel vehiclesButtonImg;
